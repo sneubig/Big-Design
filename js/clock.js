8dd100c7f43d7f5235ebe168986e7d9673847592
sneubig <neubig.stephen@gmail.com>
2018-04-18 20:18:28 -0400

    add check functions to determine angle of second hand before moving minute hand

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@
 (function() {
   // Initialise any local time clocks
   initLocalClocks();
-
+  // Start the seconds container moving
+  moveSecondHands();
+  // Set the intial minute hand container transition, and then each subsequent step
+  setUpMinuteHands();
 })();
 
 /* 
@@ -10,7 +13,7 @@
 	- Loops through each hand and applies that angle using the style.transform property of rotateZ
 
 */
-function initLocalClocks() {
+function initLocalClocks(){
 	// Get the local time using JS
 	var date = new Date;
 	var seconds = date.getSeconds();
@@ -49,4 +52,45 @@ function initLocalClocks() {
 	}
 }
 
-func
+
+/*
+ * Set a timeout for the first minute hand movement (less than 1 minute), then rotate it every minute after that
+ */
+function setUpMinuteHands(){
+	// Find out how far into the minute we are
+	var containers = document.querySelectorAll('.minutes-container');
+	var secondAngle = containers[0].getAttribute("data-second-angle");
+	if(secondAngle > 0){
+		// Set a timeout until the end of the current minute, to move the hand
+		var delay = (((360 - secondAngle) / 6) + 0.1) * 1000;
+
+		setTimeout(function() {
+			moveMinuteHands(containers);
+		}, delay);
+	}
+}
+
+
+/*
+ * Do the first minute's rotation
+ */
+function moveMinuteHands(containers){
+	for(var i=0; i < containers.length; i++){
+		containers[i].style.webkitTransform = 'rotateZ(6deg)';
+		containers[i].style.transform = 'rotateZ(6deg)';
+	}
+
+	// Then continue with a 60 second interval
+	setInterval(function() {
+		for(var i=0; i<containers.length; i++){
+			if(containers[i].angle === undefined){
+				containers[i].angle = 12;
+			}else{
+				containers[i].angle += 6;
+			}
+
+			containers[i].style.webkitTransform = 'rotateZ(' + containers[i].angle + 'deg)';
+			containers[i].style.transform = 'rotateZ(' + containers[i].angle + 'deg)';
+		}
+	}, 6000);
+}
