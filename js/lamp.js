68a33f94996c358a86c1606be82056503be7334c
sneubig <neubig.stephen@gmail.com>
2018-04-25 20:32:59 -0400

    cross off lamp instruction when task is completed

--- js/lamp.js
+++ js/lamp.js
@@ -4,7 +4,15 @@ $( document ).ready(function() {
 	$('.switch').on('click', function(){
 		$('.bulb').toggleClass('light-on');
 		$('.light-beam').toggle();
+		var lampInstruction = document.getElementById('lampInstruction');
+		console.log('lampInstruction: ' + lampInstruction);
+		var lampInstructionMessage = lampInstruction.innerHTML;
+		console.log('lampInstructionMessage: ' + lampInstructionMessage);
+		$(lampInstruction).css('text-decoration', 'line-through');
+		console.log('lampInstructionMessage AFTER: ' + lampInstructionMessage);
 	});
+
+
 
 });
 
